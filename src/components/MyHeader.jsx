ead2acfb5a796a5c6aa4098d97a0ef8095a4e713
ClaudiaSgalippa <claudia.sgappy@gmail.com>
2025-09-25 17:37:02 +0200

    Impostazione navbar

--- src/components/MyHeader.jsx
+++ src/components/MyHeader.jsx
@@ -1,3 +1,25 @@
-export default function MyHeader() {
-return (<header><h1>Sono l'header</h1></header>);
-}
+import logoDC from "../img/dc-logo.png"; {/*Importiamo l'immagine del logo*/}
+
+const MyHeader = () => { {/*Creiamo la costante per l'header*/}
+
+    const navUpper = ["CHARACTERS","COMICS","MOVIES","TV","GAMES", "COLLECTIBLES", "VIDEOS", "FANS", "NEWS", "SHOP"]; {/*Creaiamo l'array per la navigazione*/}
+
+    return ( 
+        <>
+            <header>
+                <div className="fullHeaderContainer">
+                    <img className="logo" src={logoDC} alt="Logo DC" />
+                    <nav>
+                        <ul className="navSite">
+                            {navUpper.map ((navUp, index) => ( 
+                                <li>{navUp}</li>
+                            ))}
+                        </ul>
+                    </nav>
+                </div>
+            </header>
+        </>
+    );
+}; {/*Abbiamo usato Map per creare un nuovo array che contiene gli <li> */}
+
+export default MyHeader;
