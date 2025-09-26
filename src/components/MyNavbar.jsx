8bab09ea8754b9c8769c55a22812824b95a5e5db
ClaudiaSgalippa <claudia.sgappy@gmail.com>
2025-09-26 22:54:03 +0200

    Impostazione MyNavbar

--- src/components/MyNavbar.jsx
+++ src/components/MyNavbar.jsx
@@ -1,3 +1,48 @@
-export default function MyNavbar() {
-return (<header><h1>Sono la navbar</h1></header>);
+const MyNavbar = () => {
+
+    const dcComics = ["Characters", "Comics", "Movies", "TV", "Games", "Videos", "News"];
+    const dcShop = ["Shop DC", "Shop DC Collectibles"];
+    const dcDc = ["Terms Of Use", "Privacy policy News", "Ad Choices", "Adversting", "Jobs", "Subscription", "Talent Workshops", "CPSC Certificates", "Ratings", "Shop Help", "Contact Us"];
+    const dcSites = ["DC", "MAD Magazine", "DC Kids", "DC Universe", "DC Power Visa"];
+
+    return (
+       <section className="allLinksSites">
+            <div className="containerSites">
+                <div>
+                    <h2>DC COMICS</h2>
+                    <ul>
+                        {dcComics.map((navDown, index) => (
+                            <li key={index}><a href="#">{navDown}</a></li>
+                        ))}
+                    </ul>
+                </div>
+                <div>
+                    <h2>SHOP</h2>
+                    <ul>
+                        {dcShop.map((navDown, index) => (
+                            <li key={index}><a href="#">{navDown}</a></li>
+                        ))}
+                    </ul>
+                </div>
+                <div>
+                    <h2>DC</h2>
+                    <ul>
+                        {dcDc.map((navDown, index) => (
+                            <li key={index}><a href="#">{navDown}</a></li>
+                        ))}
+                    </ul>
+                </div>
+                <div>
+                    <h2>SITES</h2>
+                    <ul>
+                        {dcSites.map((navDown, index) => (
+                            <li key={index}><a href="#">{navDown}</a></li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+       </section> 
+    )
 }
+
+export default MyNavbar
